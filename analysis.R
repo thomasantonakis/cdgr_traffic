f0948140d0d663fccb43d3f7177c52fb623ba7c6
Thomas <thomas.antonakis@gmail.com>
2015-01-21 19:10:11 +0200

    Create some Plots. Refine, leave line types open

--- analysis.R
+++ analysis.R
@@ -61,30 +61,36 @@ par(mfrow=c(3,1))
 # Regarding the ratios, a stacked 100% bar plot 
 # or a line plot as below
 
-plot(report2$se_vst_pct, type="l", lwd=2, col="blue", ylim=c(0, 1), xaxs="i", yaxs="i", 
+plot(report2$se_vst_pct, type="p", lwd=2, col="blue", ylim=c(0, 1), xaxs="i", yaxs="i", 
      main = "Visits share %", ylab= "Share % of Visits", xlab="Weeks starting January 2014")
-lines(report2$pd_vst_pct, lwd=2, col="red")
-lines(report2$di_vst_pct, lwd=2, col="black")
-lines(report2$ot_vst_pct, lwd=2, col="green")
+lines(report2$pd_vst_pct, lwd=2,type="l", col="red")
+lines(report2$di_vst_pct, lwd=2, type="o",col="black")
+lines(report2$ot_vst_pct, lwd=2, type="b",col="green")
 legend("topleft", legend=c("Search","Paid", "Direct", "Other"), lwd=c(2,2,2,2), col=c("blue","red", "black", "green"))
 
-plot(report2$se_reg_pct, type="l", lwd=2, col="blue", ylim=c(0, 1), xaxs="i", yaxs="i", 
+plot(report2$se_reg_pct, type="c", lwd=2, col="blue", ylim=c(0, 1), xaxs="i", yaxs="i", 
      main = "Registrations share %", ylab= "Share % of Registrations", xlab="Weeks starting January 2014")
-lines(report2$pd_reg_pct, lwd=2, col="red")
-lines(report2$di_reg_pct, lwd=2, col="black")
-lines(report2$ot_reg_pct, lwd=2, col="green")
+lines(report2$pd_reg_pct, lwd=2, type="s",col="red")
+lines(report2$di_reg_pct, lwd=2, type="S",col="black")
+lines(report2$ot_reg_pct, lwd=2, type="h",col="green")
 legend("topleft", legend=c("Search","Paid", "Direct", "Other"), lwd=c(2,2,2,2), col=c("blue","red", "black", "green"))
 
-plot(report2$se_ord_pct, type="l", lwd=2, col="blue", ylim=c(0, 0.6), xaxs="i", yaxs="i", 
+plot(report2$se_ord_pct, type="c", lwd=2, col="blue", ylim=c(0, 0.6), xaxs="i", yaxs="i", 
      main = "Orders share %", ylab= "Share % of Orders", xlab="Weeks starting January 2014")
-lines(report2$pd_ord_pct, lwd=2, col="red")
-lines(report2$di_ord_pct, lwd=2, col="black")
-lines(report2$ot_ord_pct, lwd=2, col="green")
+lines(report2$pd_ord_pct,  type="c",lwd=2, col="red")
+lines(report2$di_ord_pct,  type="c",lwd=2, col="black")
+lines(report2$ot_ord_pct,  type="c",lwd=2, col="green")
 legend("topleft", legend=c("Search","Paid", "Direct", "Other"), lwd=c(2,2,2,2), col=c("blue","red", "black", "green"))
 
-# df <- melt(df, id="x")  # convert to long format
-# ggplot(df,aes(x=x, y=value, colour=variable)) +
+# require(ggplot2)
+# report3 <- melt(report2, id="x")  # convert to long format
+# ggplot(report3,aes(x=x, y=value, colour=variable)) +
 #         geom_line()
 
-# require(lattice)
-# xyplot(df$x ~ df[,2] + df[,3], type = c('l','l'), col = c("blue", "red"), auto.key=T)
+require(lattice)
+
+# xyplot(report2$se_vst_pct + report2$pd_vst_pct + report2$di_vst_pct+ report2$ot_vst_pct ~ rownames(report2) , type = c('l','l'), 
+#        col = c("blue", "red", "black", "green") , auto.key=T)
+
+xyplot(report2$se_vst_pct + report2$pd_vst_pct + report2$di_vst_pct+ report2$ot_vst_pct ~ report$Week , type = c('l','l'), 
+       col = c("blue", "red", "black", "green") , auto.key=T)
