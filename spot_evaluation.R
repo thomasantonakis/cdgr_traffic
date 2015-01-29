c52a4a05f191d12f8675aa1e3d66211ea26f1bde
Thomas <thomas.antonakis@gmail.com>
2015-01-29 13:02:14 +0200

    Successful allocation

--- spot_evaluation.R
+++ spot_evaluation.R
@@ -114,61 +114,73 @@ proc.time() - ptm
 for (i in 1:nrow(permin)){
         for (j in 1:nrow(actspots)){
                 dif<- difftime(permin$timestamp[i], actspots$Time[j], units="mins")
-                if ( (dif>=-1) & (dif<=10)) {
+                if ( (dif>=-0.1) & (dif<=3.1)) {
                         permin$affected[i]<- permin$affected[i]+1
                         #permin$cost[i]<-permin$cost[i] + actspots$Cost[j]
                 } 
         }
 }
+
+affected<-permin[permin$affected!=0,]
 proc.time() - ptm
 
-#sessions per 3 minutes
-#registrations per 3 minutes
+#sessions per 0-3 minutes
+#registrations per 2-10 minutes
 
 
-# Spot approach
-actspots$sessions<-NULL
-actspots$registrations<-0
-for (i in 1:nrow(actspots)){
-#         for (j in 1:nrow(actspots)) {
-#                 actspots$dif <- difftime(actspots$Time[i], actspots$Time[j], units="mins")
-#                 actspots$w <- actspots$GRP / sum(actspots$GRP[])
-#                if (i!=j) {
-                        permin$dif<- difftime(permin$timestamp, actspots$Time[i], units="mins")
-                        actspots$sessions[i]<-sum(permin$sessions[permin$dif>=-0.5 & permin$dif<=3])
-                        actspots$registrations[i]<-sum(permin$registrations[permin$dif>=-0.5 & permin$dif<=10])  
-                        permin$dif<-NULL
-#                }
-#        }
-}
+# Spot approach but over estimating (doublecounting overlapped minutes)
+# actspots$sessions<-0
+# actspots$registrations<-0
+# for (i in 1:nrow(actspots)){
+#         permin$dif<- difftime(permin$timestamp, actspots$Time[i], units="mins")
+#         actspots$sessions[i]<-sum(permin$sessions[permin$dif>=-0.5 & permin$dif<=3])
+#         actspots$registrations[i]<-sum(permin$registrations[permin$dif>=-0.5 & permin$dif<=10])  
+#         permin$dif<-NULL
+# }
 
 ###############################
 # Way of thinking
 ###############################
 
 # actcosts 
+actspots$sessions<-0
+actspots$registrations<-0
 # gia kathe spot
-# for (i in 1:nrow(actspots)){
+for (i in 1:nrow(actspots)){
         # gia kathe lepto
-        # for (j in 1:11) { 
-                # dhmio;yrgise wra anaforas
+        for (j in 0:10) { 
+                # dhmioyrgise wra anaforas
+                now<- actspots$Time[i] +minutes(j)
                 # check poia spot einai energa
-                # energo einai ena spo pou to dif time time stamp tou me wra anaforas einai mikrotero tou 0-10
+                # energo einai ena spot pou to dif time time stamp tou me wra anaforas einai mikrotero tou 0-10
                 # o eaftos mou prepei na einai TRUE
                 # athtoise ta GRPs tous (krata denominator)
+                if (j>1){
+                        denomreg<- sum(actspots$GRP[(difftime(now, actspots$Time, units="mins")>=1.9 & difftime(now, actspots$Time, units="mins")<=10.1)])
+                wreg<- actspots$GRP[i] / denomreg
+                meritreg<- wreg * sum(permin$registrations[abs(difftime(now, permin$timestamp, units="mins"))<=0.5])
+                actspots$registrations[i] <- actspots$registrations[i] + meritreg
+                }
                 # athroise sessions kai ta registrations twn energwn
+                if (j<=3){
+                        denomses<- sum(actspots$GRP[(difftime(now, actspots$Time, units="mins")>=-0.1 & difftime(now, actspots$Time, units="mins")<=3.1)])
+                wses<- actspots$GRP[i] / denomses
+                meritses<- wses * sum(permin$sessions[abs(difftime(now, permin$timestamp, units="mins"))<=0.5])
+                actspots$sessions[i] <- actspots$sessions[i] + meritses
+                }
                 # grapse sth sthli sessions kai registrations apo panw
                 # pollaplasiase ta sessions * GRPs spot / denominator
                 # an einai ena mono energo grps/ denominator =1 ara olo to lepto
-#        }
-# }
+       }
+}
 
+# Computations on actspots data-frame
 
 
 # ga:channelGrouping
 
 
-affected<-permin[permin$affected!=0,]
+
 
 write.xlsx(x = affected, file = "affected.xlsx", row.names = FALSE)
 write.xlsx(x = permin, file = "permin.xlsx", row.names = FALSE)
